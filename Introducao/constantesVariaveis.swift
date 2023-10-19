30ee959284f69939db7e00f392988382f8c200da
Marcelo <marcelo@siasp.com.br>
2023-10-19 08:14:08 -0300

    Introducao: Constantes e Variaveis

---
+++ Introducao/constantesVariaveis.swift
@@ -0,0 +1,21 @@
+// Declarações de Constatnets e Variáveis
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttemps = 0
+
+// Anotações de Tipo
+var welcomeMessage: String
+welcomeMessage = "Hello"
+var red, green, blue: Double
+
+// Nomenclatura
+var teste1 = "teste"
+let `public` = "Geral"
+let pi = 3.14159
+let icone = "dogcow"
+var frendlyWelcome = "Hello!"
+frendlyWelcome = "Bonjuor!"
+
+// Imprimindo Valores
+print(frendlyWelcome, terminator: "")
+print(frendlyWelcome)
+print("O valor atual é: \(frendlyWelcome)")
